5aef6b4c53c49444c41baa441eeade2c9d6b82a1
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2023-11-22 03:28:46 +0900

    Design : 버튼 공통 컴포넌트 생성

---
+++ src/components/commonButton/CommonButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./commonButton.scss";
+
+interface ButtonType {
+  text: string;
+  size?: string;
+  shape?: string;
+  colorName?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+function CommonButton({ text, size, shape, colorName, onClick }: ButtonType) {
+  return (
+    <button
+      className={`CommonButton ${size} ${shape}-${colorName}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+CommonButton.defaultProps = {
+  size: "small",
+  shape: "fill",
+  colorName: "coral500",
+};
+
+export default CommonButton;
