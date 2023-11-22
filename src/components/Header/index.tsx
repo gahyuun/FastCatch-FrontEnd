c7f430990df1e881387e45717650cd334521f33c
Bumang-Cyber <jove0729@naver.com>
2023-11-22 20:08:33 +0900

    fix: merge후 commonButton props 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -27,7 +27,7 @@ const Header = () => {
           {isLoggedIn ? (
             <MyInfo></MyInfo> //
           ) : (
-            <CommonButton text="로그인" size="small" shape="fill" colorName="coral500" onClick={loginHandler} />
+            <CommonButton text="로그인" buttonSize="small" shape="fill" colorName="coral500" onClick={loginHandler} />
           )}
         </section>
       </div>
