781bab9664455f20d65a8c9fc864a19d77770619
Eojoonhyuk <anftlcb@gmail.com>
2023-11-24 00:18:46 +0900

    Docs: SelectedRoomItem & SelectedAccomodation 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as CommonBadge } from "./commonBadge/CommonBadge";
 export { default as CommonButton } from "./commonButton/CommonButton";
 export { default as CommonInput } from "./commonInput/CommonInput";
+export { default as SelectedRoomItem } from "./selectedRoomItem/SelectedRoomItem";
+export { default as SelectedAccomodation } from "./selectedAccomodation/SelectedAccomodation";
