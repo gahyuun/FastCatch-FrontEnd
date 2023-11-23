00fe03cf493142d1d8ec63f006347c8824a9334f
Eojoonhyuk <anftlcb@gmail.com>
2023-11-24 00:21:23 +0900

    Feat: 마이페이지와 공통인 레이아웃 컴포넌트화

---
+++ src/components/selectedAccomodation/SelectedAccomodation.tsx
@@ -0,0 +1,15 @@
+import { SelectedRoomItem } from "..";
+import "./SelectedAccomodation.scss";
+
+const SelectedAccomodation = () => {
+  return (
+    <section className="selected-room-container">
+      <h2 className="text-subtitle4">강남 호텔 클릭</h2>
+      <div className="item-list">
+        <SelectedRoomItem />
+      </div>
+    </section>
+  );
+};
+
+export default SelectedAccomodation;
