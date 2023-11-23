fdf466e4934d8b65ab048cccc7e1f7e2d4ea6eea
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2023-11-23 17:08:33 +0900

    Design : 숙소 옵션 디자인 추가

--- src/pages/accommodation/accommodationOptions/AccommodationOptions.tsx
+++ src/pages/accommodation/accommodationOptions/AccommodationOptions.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import "./accommodationOptions.scss";
+import englishToKoreanFormat from "../../../utils/englishToKoreanFormat";
+import { TbSwimming, TbWifi } from "react-icons/tb";
+import { FaSmoking } from "react-icons/fa";
+import { BiDumbbell } from "react-icons/bi";
+import {
+  MdLocalParking,
+  MdOutlineFreeBreakfast,
+  MdRestaurant,
+} from "react-icons/md";
+import { PiCookingPotBold, PiDogBold } from "react-icons/pi";
+
+// import { IconName } from "react-icons/io5";
 
 const AccommodationOptions = ({ accommodationOptions }: any) => {
-  // console.log(accommodationOptions);
-  // let trueKeys: any = [];
-  // if (accommodationOptions) {
-  //   trueKeys = Object.keys(accommodationOptions).filter(
-  //     (key) => accommodationOptions[key] === true
-  //   );
-  // }
-
-  // console.log(trueKeys);
+  const template: any = {
+    has_smoking_room: ["흡연 가능", <FaSmoking size="50px" />],
+    has_pet_room: ["반려견 동반", <PiDogBold size="50px" />],
+    has_parking_lot: ["주차 가능", <MdLocalParking size="50px" />],
+    has_wifi: ["와이파이", <TbWifi size="50px" />],
+    has_swimming_pool: ["수영장", <TbSwimming size="50px" />],
+    has_gym: ["헬스장", <BiDumbbell size="50px" />],
+    has_breakfast: ["조식", <MdOutlineFreeBreakfast size="50px" />],
+    has_restaurant: ["레스토랑", <MdRestaurant size="50px" />],
+    has_cooking_room: ["취사 가능", <PiCookingPotBold size="50px" />],
+  };
+  console.log(englishToKoreanFormat(accommodationOptions, template));
 
   return (
     <div className="accommodation__options">
@@ -18,8 +33,16 @@ const AccommodationOptions = ({ accommodationOptions }: any) => {
         <span className="text-subtitle4">숙소 옵션</span>
       </div>
       <div className="accommodation__options-box">
-        {/* {trueKeys &&
-          trueKeys.map((option: any) => <div key={option}> {option} - </div>)} */}
+        <div className="accommodation__options-box__grid">
+          {englishToKoreanFormat(accommodationOptions, template).map(
+            (option: any) => (
+              <div className="accommodation__option" key={option}>
+                {option[1]}
+                <span>{option[0]}</span>
+              </div>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
