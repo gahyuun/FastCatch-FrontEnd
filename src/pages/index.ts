fd4203a6e09476f4c2cd165f892ffe15a0037ff1
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2023-11-25 11:39:54 +0900

    Docs : 파일명 대소문자 변경을 위한 임시 파일명으로 수정

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export { default as Basket } from "@/src/pages/basket/Basket";
 export { default as Home } from "@/src/pages/home";
-export { default as Accommodation } from "@/src/pages/accommodation/accommodation";
+export { default as Accommodation } from "@/src/pages/accommodation/Accommodation_";
 export { default as Order } from "@/src/pages/order/Order";
 export { default as Members } from "@/src/pages/members/Members";
 export { default as OrderList } from "@/src/pages/orderList/OrderList";
