c66aff67986a14ebe522eac78abe5dd7387b22f3
고솔미 (Solmi Koh) <113079102+SOL-MI@users.noreply.github.com>
2023-11-27 01:33:47 +0900

    Feat : useQuery를 이용해 get 수행하도록 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { worker } from "./mocks/browsers.ts";
+
+const queryClient = new QueryClient();
 
 if (process.env.NODE_ENV === "development") {
   worker.start();
@@ -10,6 +13,8 @@ if (process.env.NODE_ENV === "development") {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/accommodation/Accommodation_.tsx
+++ src/pages/accommodation/Accommodation_.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./accommodation.scss";
 import axios from "axios";
+import { useQuery } from "react-query";
 import RoomSelect from "./roomSelect/RoomSelect";
 import AccommodationMainInfo from "./accommodationMainInfo/AccommodationMainInfo";
 import AccommodationIntroduce from "./accommodationIntroduce/AccommodationIntroduce";
@@ -8,45 +9,72 @@ import AccommodationOptions from "./accommodationOptions/AccommodationOptions";
 import AccommodationImgSwiper from "./accommodationImgSwiper/AccommodationImgSwiper";
 
 const Accommodation = () => {
-  const [data, setData]: any = useState([]);
+  const fetchListData = async () => {
+    try {
+      const res = await axios.get(
+        "../../../public/data/accommodationDetail.json"
+      );
+      return res.data;
+    } catch (error) {
+      throw new Error("Failed to fetch data");
+    }
+  };
 
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios({
-          method: "get",
-          url: "/accommodation",
-        });
-        // console.log(res.data[0]);
-        if (res.status === 200) {
-          await setData(res.data[0]);
-        }
-        setIsLoaded(!isLoaded);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-
-    // const postHotel = async () => {
-    //   try {
-    //     const response = await axios({
-    //       method: "post",
-    //       url: "/accommodation",
-    //     });
-    //     console.log("post 성공", response.data);
-    //   } catch (error) {
-    //     console.log("post도중 에러 발생", error);
-    //   }
-    // };
-    // postHotel();
-  }, []);
-
-  if (!isLoaded) {
-    return <div>로딩 화면</div>;
+  const { data, error, isError, isLoading }: any = useQuery({
+    queryKey: ["postDetail"],
+    queryFn: fetchListData,
+    staleTime: 500000,
+    select: (data) => {
+      const accommodationValue = data[0];
+      return accommodationValue;
+    },
+  });
+  if (isLoading) {
+    return <div>로딩중..!!!!!</div>;
   }
+  if (isError) {
+    return <div>여기는 에러 페이지!!!!!!!!!!! {error.message}</div>;
+  }
+
+  // const [data, setData]: any = useState([]);
+
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await axios({
+  //         method: "get",
+  //         url: "/accommodation",
+  //       });
+  //       // console.log(res.data[0]);
+  //       if (res.status === 200) {
+  //         await setData(res.data[0]);
+  //       }
+  //       setIsLoaded(!isLoaded);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   fetchData();
+
+  //   // const postHotel = async () => {
+  //   //   try {
+  //   //     const response = await axios({
+  //   //       method: "post",
+  //   //       url: "/accommodation",
+  //   //     });
+  //   //     console.log("post 성공", response.data);
+  //   //   } catch (error) {
+  //   //     console.log("post도중 에러 발생", error);
+  //   //   }
+  //   // };
+  //   // postHotel();
+  // }, []);
+
+  // if (!isLoaded) {
+  //   return <div>로딩 화면</div>;
+  // }
 
   return (
     <div className="accommodation-container">
