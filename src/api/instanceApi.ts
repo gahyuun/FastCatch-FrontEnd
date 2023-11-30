59b3d246aa4b5e3a4572c2468085cfa9736470e3
Taegwan Youn <tkyoun0421@naver.com>
2023-12-01 03:27:54 +0900

    comment: 주석 제

--- src/api/instanceApi.ts
+++ src/api/instanceApi.ts
@@ -1,9 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 import { getToken } from "../utils/getToken";
 
-// const accessToken =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjY2NjQG5hdmVyLmNvbSIsImlhdCI6MTcwMTMyODA3NSwiZXhwIjoxNzAxNDE0NDc1fQ.5fVvglqa1ZfwDu7ztQ5Tzc-vNexO-lp6OHvNhaDcPPiiEoiCz7IuyhXjH5ZJDXgN4Bi0RGpGVslk0p4Nj3D2Tg";
-
 const instance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL_TEST_SERVER,
   timeout: 5000,
