b1721170c5d08c8941711c718fbb60ea61fa28d6
Taegwan Youn <tkyoun0421@naver.com>
2023-12-01 03:27:17 +0900

    feat: getToken 함수 추가

--- src/api/instanceApi.ts
+++ src/api/instanceApi.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
+import { getToken } from "../utils/getToken";
 
-const accessToken =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjY2NjQG5hdmVyLmNvbSIsImlhdCI6MTcwMTMyODA3NSwiZXhwIjoxNzAxNDE0NDc1fQ.5fVvglqa1ZfwDu7ztQ5Tzc-vNexO-lp6OHvNhaDcPPiiEoiCz7IuyhXjH5ZJDXgN4Bi0RGpGVslk0p4Nj3D2Tg";
+// const accessToken =
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjY2NjQG5hdmVyLmNvbSIsImlhdCI6MTcwMTMyODA3NSwiZXhwIjoxNzAxNDE0NDc1fQ.5fVvglqa1ZfwDu7ztQ5Tzc-vNexO-lp6OHvNhaDcPPiiEoiCz7IuyhXjH5ZJDXgN4Bi0RGpGVslk0p4Nj3D2Tg";
 
 const instance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL_TEST_SERVER,
@@ -11,21 +12,19 @@ const instance: AxiosInstance = axios.create({
 instance.interceptors.request.use(
   config => {
     config.headers["Content-Type"] = "application/json";
-    config.headers["Authorization"] = `Bearer ${accessToken}`;
+    const accessToken = getToken();
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
     https: return config;
   },
   error => {
-    console.log(error);
     return Promise.reject(error);
   }
 );
 
 instance.interceptors.response.use(
   response => {
-    // if (response.status === 404) {
-    //   console.log("404 페이지로 넘어가야 함!");
-    // }
-
     return response;
   },
   async error => {
