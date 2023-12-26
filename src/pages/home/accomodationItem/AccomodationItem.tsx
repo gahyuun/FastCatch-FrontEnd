5951aca256d1fd6f509c332d2cc51df5bd50843d
박가현 <81469686+gahyuun@users.noreply.github.com>
2023-12-26 17:06:46 +0900

    Style: 숙소 아이템 퍼블리싱
    * Style: pretendard 적용 문제 해결
    
    * Style: 숙소 아이템 가격 퍼블리싱 수정
    
    * Chore: number format 함수 제거
    
    * Style: 숙소 이미지 내 쿠폰 박스 구현
    
    * Chore: 뱃지 삭제

--- src/pages/home/accomodationItem/AccomodationItem.tsx
+++ src/pages/home/accomodationItem/AccomodationItem.tsx
@@ -1,9 +1,7 @@
 import "./accomodationItem.scss";
-import numberFormat from "@/utils/numberFormat";
 import { useNavigate } from "react-router-dom";
 import { accommodationCategoryData } from "@/constant/categories";
 import { Accommodation } from "../../../types/accommodations";
-import { Badge } from "@/components/common";
 interface accommodationProps {
   data: Accommodation;
 }
@@ -17,31 +15,33 @@ const AccommodationItem = ({ data }: accommodationProps) => {
 
   return (
     <div className="accomdationItem-container" onClick={navigateHandler}>
-      <div className="accomdationItem-container__image-box">
-        <img
-          src={`https://fastcatch-image.s3.ap-northeast-2.amazonaws.com/${data.image}`}
-        />
+      <div
+        className="accomdationItem-container__image-box"
+        style={{
+          backgroundImage: `url(https://fastcatch-image.s3.ap-northeast-2.amazonaws.com/${data.image})`,
+        }}
+      >
+        <div className="accomdationItem-container__coupon-box">
+          10,000원 즉시할인
+        </div>
       </div>
       <div className="accomdationItem-container__desc-box">
         <div className="item-info">
           <div>
             <strong className="text-subtitle5">{data.name}</strong>
             <div className="text-body2 item-info__location">
-              <p>
-                {accommodationCategoryData[data.category]} |{" "}
-                {data.address?.split(" ").slice(0, 3).join(" ") || data.region}
-              </p>
+              <p>{accommodationCategoryData[data.category]} </p>
             </div>
-          </div>
-          <div className="item-info__status">
-            {data.soldOut && <Badge text="예약마감" badgeType="fill" />}
           </div>
         </div>
         <div className="item-price">
-          <span className="text-body1">최저가</span>
-          <div className="text-subtitle5">
-            <span>{numberFormat(data.lowestPrice)}</span>
-            <span className="won">원</span>
+          {/* <span className="text-body1">최저가</span> */}
+          <div className="coupon-box">쿠폰가</div>
+          <div className="price-container">
+            <div className="price"> {data.lowestPrice.toLocaleString()}원</div>
+            <div className="lowest-price">
+              {data.lowestPrice.toLocaleString()}원
+            </div>
           </div>
         </div>
       </div>
