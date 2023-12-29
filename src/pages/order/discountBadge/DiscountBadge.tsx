43341b532f0db1fa12b8c8683ddc4710b8239706
gahyuun <pgh123166@naver.com>
2023-12-29 06:18:09 +0000

    Style: 할인 쿠폰 선택 시 할인 가격 badge 추가 (#38)
    * Chore: 원격 저장소 최신 버전으로 업데이트
    
    * Style: border 그라디언트 좌우 수정

---
+++ src/pages/order/discountBadge/DiscountBadge.tsx
@@ -0,0 +1,15 @@
+import "./discountBadge.scss";
+
+const DiscountBadge = () => {
+  return (
+    <>
+      <div className="discount-badge-container">
+        <div className="discount-badge">
+          <p>총 20,000원 할인 받았어요!</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DiscountBadge;
