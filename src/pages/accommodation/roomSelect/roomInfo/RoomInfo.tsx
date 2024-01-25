2a38e1cc53a7e1c804e120102b5ec16ad9828637
gahyuun <pgh123166@naver.com>
2024-01-25 08:47:49 +0000

    Fix: Navigate 추가. (#61)

--- src/pages/accommodation/roomSelect/roomInfo/RoomInfo.tsx
+++ src/pages/accommodation/roomSelect/roomInfo/RoomInfo.tsx
@@ -1,4 +1,5 @@
 import { Badge, Button, ToastLayout } from "@/components/common";
+import { PATH } from "@/routes/constants";
 import { filterState } from "@/states/filterState";
 import { orderState } from "@/states/orderState";
 import { userState } from "@/states/userState";
@@ -128,8 +129,8 @@ const RoomInfo = ({ room, accommodationName, isClicked }: RoomInfoProps) => {
         options: roomOption,
       },
     ]);
+    navigate(PATH.ORDER);
   };
-
   let text = "";
   if (count === 0 || soldOut) {
     text = "판매된 객실입니다";
