2464f52262c07d2032ea56b7715b95e202b15482
gahyuun <pgh123166@naver.com>
2024-01-25 08:50:34 +0000

    Feat: 페이지간 이동 시 스크롤 탑 커스텀 훅 생성 (#62)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/common";
+import { ScrollToTop } from "./hooks/useScrollToTop";
 
 export default function App() {
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Outlet />
     </div>
