e05c36d6e749a60a0917d25f08d15b9fe6bae525
gahyuun <pgh123166@naver.com>
2024-01-25 09:26:49 +0000

    Fix: 인터셉터 수정 (#64)

--- src/api/instanceApi.ts
+++ src/api/instanceApi.ts
@@ -1,36 +1,18 @@
 import axios, { AxiosInstance } from "axios";
-
-import { isAccessTokenExpired } from "@/utils/checkToken";
 import { getCookie, removeCookie, setCookie } from "@/utils/cookies";
 const instance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 instance.interceptors.request.use(
   async config => {
-    config.headers["Content-Type"] = "application/json";
     const accessToken = localStorage.getItem("accessToken");
-    const refreshToken = getCookie("refreshToken");
     if (accessToken) {
-      const isTokenExpired = isAccessTokenExpired(accessToken);
-      if (isTokenExpired) {
-        try {
-          const res = await instance.post("/api/auth/refresh", {
-            accessToken: accessToken,
-            refreshToken: refreshToken,
-          });
-          const newAccessToken = res.data.accessToken;
-          const newRefreshToken = res.data.refreshToken;
-          config.headers["Authorization"] = `Bearer ${newAccessToken}`;
-          localStorage.setItem("accessToken", newAccessToken);
-          setCookie(newRefreshToken);
-        } catch (refreshError) {
-          console.log(refreshError);
-        }
-      } else {
-        config.headers["Authorization"] = `Bearer ${accessToken}`;
-      }
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
     }
     return config;
   },
@@ -41,17 +23,31 @@ instance.interceptors.request.use(
 
 instance.interceptors.response.use(
   response => response,
-  error => {
-    if (
-      error.response?.status === 401 &&
-      window.location.pathname !== "login" &&
-      window.location.pathname !== "signup"
-    ) {
-      localStorage.clear();
-      removeCookie();
-      setTimeout(() => {
-        window.location.replace("/login");
-      }, 1000);
+  async error => {
+    if (error.response?.status === 401) {
+      if (error.response?.data && "code" in error.response.data) {
+        // 리프레시 토큰이 유효하지 않을 때
+        if (error.response.data.code === 1001) {
+          alert("로그인 만료입니다.");
+          localStorage.clear();
+          removeCookie();
+          window.location.replace("/login");
+        }
+      } else {
+        const accessToken = localStorage.getItem("accessToken");
+        if (accessToken) {
+          const res = await instance.post("/api/auth/refresh", {
+            accessToken: accessToken,
+            refreshToken: getCookie("refreshToken") as string,
+          });
+          localStorage.setItem("accessToken", res.data.accessToken);
+          setCookie(res.data.refreshToken);
+        } else {
+          localStorage.removeItem("accessToken");
+          removeCookie();
+          window.location.replace("/login");
+        }
+      }
     }
     return Promise.reject(error);
   }
